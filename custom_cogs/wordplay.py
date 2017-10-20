2cf6f6cc12e98df98630de0d8eaacb4ef000a2a7
Tyler Park <tyler.park@fortemtech.com>
2017-10-20 12:59:43 -0600

    Basic wordsmith frequency implemented.
    Signed-off-by: Tyler Park <tyler.park@fortemtech.com>

--- custom_cogs/wordplay.py
+++ custom_cogs/wordplay.py
@@ -1,5 +1,6 @@
 import discord
 import io
+import re
 
 from discord.ext import commands
 from cogs.utils.checks import load_optional_config, get_google_entries, embed_perms
@@ -16,10 +17,28 @@ class WordSmith:
             await ctx.send(self.bot.bot_prefix + 'Invalid Syntax. See `>help wordsmith` for more info on how to use this command.')
 
     @wordsmith.command(pass_context=True, name="frequency")
-    async def angry(self, ctx):
-        # client = discord.Client()
-        logs = self.bot.logs_from("testing")
-        # print(help(self.bot))
+    async def frequency(self, ctx):
+        try:
+            messages = await ctx.history(limit=1000).flatten()
+            wordDict = {}
+            for msg in messages:
+                for word in re.sub(r'([^\s\w]|_)+', '', msg.content).lower().split():
+                    if word in wordDict:
+                        wordDict[word] += 1
+                    else:
+                        wordDict[word] = 1
+            await ctx.send(self.bot.bot_prefix + 'Frequency report over 1000 messages:')
+            count = 0
+            for key, val in sorted(wordDict.items(), key=lambda x: (-x[1],x[0])):
+                if key in banned:
+                    continue
+                count += 1
+                await ctx.send('\t{0}: {1}'.format(key, val))
+                if count == 10:
+                    break
+
+        except Exception as e:
+            print('{}'.format(e))
         
 
 
@@ -27,9 +46,103 @@ class WordSmith:
 def setup(bot):
     bot.add_cog(WordSmith(bot))
 
-    client = discord.Client()
-    try:
-        client.logs_from
-        print('it works')
-    except:
-        print('what??')
+banned = '''a
+about
+all
+also
+and
+as
+at
+be
+because
+but
+by
+can
+come
+could
+day
+do
+even
+find
+first
+for
+from
+get
+give
+go
+have
+he
+her
+here
+him
+his
+how
+I
+if
+in
+into
+it
+its
+just
+know
+like
+look
+make
+man
+many
+me
+more
+my
+new
+no
+not
+now
+of
+on
+one
+only
+or
+other
+our
+out
+people
+say
+see
+she
+so
+some
+take
+tell
+than
+that
+the
+their
+them
+then
+there
+these
+they
+thing
+think
+this
+those
+time
+to
+two
+up
+use
+very
+want
+way
+we
+well
+what
+when
+which
+who
+will
+with
+would
+year
+you
+your'''
