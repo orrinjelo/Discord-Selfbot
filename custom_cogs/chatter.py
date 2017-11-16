8003e8920f24c7506c1f09d5792455e0e5e9d085
Tyler Park <tyler.park@fortemtech.com>
2017-11-16 10:33:22 -0700

    added dialogflow api

--- custom_cogs/chatter.py
+++ custom_cogs/chatter.py
@@ -5,6 +5,16 @@ import re
 from discord.ext import commands
 from cogs.utils.checks import load_optional_config, get_google_entries, embed_perms
 
+try:
+    import apiai
+except ImportError:
+    sys.path.append(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+    )
+    import apiai
+
+CLIENT_ACCESS_TOKEN = 'f83ff865da164af5bf87b8de4c815e6e'
+
 class Chatter:
     def __init__(self, bot):
         self.bot = bot
@@ -12,7 +22,19 @@ class Chatter:
     @commands.group(pass_context=True, aliases=['ch', 'chat'])
     async def chatter(self, ctx, *msg):
         '''A utility to communicate with Jelobot dialog flow'''
-        await ctx.send(' '.join(msg))
+        ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+
+        request = ai.text_request()
+
+        request.lang = 'en'  # optional, default value equal 'en'
+
+        request.session_id = "jelobot_user_11302"
+
+        request.query = ' '.join(msg)
+
+        response = request.getresponse()
+
+        await ctx.send(self.bot.bot_prefix + response.read())
 
 
 def setup(bot):
