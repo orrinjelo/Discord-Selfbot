96c4a23d97c6716075e8f401a4bbf01b2639056e
Tyler Park <tyler.park@fortemtech.com>
2017-11-16 10:25:46 -0700

    Beginning start of chat bot

---
+++ custom_cogs/chatter.py
@@ -0,0 +1,15 @@
+import discord
+import io
+import re
+
+from discord.ext import commands
+from cogs.utils.checks import load_optional_config, get_google_entries, embed_perms
+
+class Chatter:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(pass_context=True, aliases=['ch', 'chat'])
+    async def chatter(self, ctx, msg):
+        '''A utility to communicate with Jelobot dialog flow'''
+        await ctx.send(msg)
